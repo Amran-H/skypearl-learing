cd5e687be634163c44a28da8c20ae80a96a8e451
Amran Hossain <amran.h.akash@gmail.com>
2022-10-26 10:46:10 +0600

    sidebar option added

--- src/Shared/LeftSideNav/LeftSideNav.js
+++ src/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/course-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            <h2>Left Side</h2>
+            <h3>Total Courses: {categories.length}</h3>
+            <div>
+                {
+                    categories.map(category => <p className="d-grid gap-2 px-5 " >
+                        <Button variant="dark" size="lg" key={category.id}><Link>{category.name}</Link></Button>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
